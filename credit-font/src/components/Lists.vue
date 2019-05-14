52f64447cee3c9097ef3fc29eb4ce2485fabf60a
CarlsKe <fernancarldiaz@outlook.com>
2019-05-14 08:19:11 -0500

    feat: component List

---
+++ credit-font/src/components/Lists.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Login</h2>
+    <section>
+        <p>Credits</p>
+    </section>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      auth: {
+        email: null,
+        password: null
+      }
+    }
+  },
+  methods: {
+    eventList () {
+
+      axios.get('http://127.0.0.1:5000/credits/', this.auth)
+      .then((response) => {
+        this.$emit('event', value)
+      })
+      .catch((error) =>{
+        this.$emit('event', value)
+      });
+
+
+    }
+  }
+}
+</script>
